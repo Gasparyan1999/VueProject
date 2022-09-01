0f017089b7a75177edce22c37dac8255ae83bab8
Gasparyan1999 <gasparyan.george1999@gmail.com>
2022-09-01 16:18:05 +0400

    Cards

--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,26 @@
 import { defineStore } from "pinia";
 
+if (!localStorage.getItem("card"))
+  localStorage.setItem("card", JSON.stringify([]));
+
 export const useNumberStore = defineStore({
   id: "numbers",
-  state: () => ({ array: [] }),
+  state: () => ({ array: JSON.parse(localStorage.getItem("card")) }),
   actions: {
     add() {
       this.array.push({
         num: Math.floor(Math.random() * 1000),
         id: Date.now() + Math.random() * 1000,
       });
+      localStorage.setItem("card", JSON.stringify(this.array));
     },
     sort() {
       this.array.sort((a, b) => a.num - b.num);
+      localStorage.setItem("card", JSON.stringify(this.array));
     },
     remove(id) {
       this.array = this.array.filter((elem) => elem.id != id);
+      localStorage.setItem("card", JSON.stringify(this.array));
     },
   },
 });
